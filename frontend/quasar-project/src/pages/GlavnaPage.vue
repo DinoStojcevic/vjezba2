3ef51af56aea34fe4c3fad58d48f10a2c218bb99
DinoStojcevic <dinoskola0@gmail.com>
2024-12-04 23:29:08 +0100

    asdfsa

---
+++ frontend/quasar-project/src/pages/GlavnaPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page padding>
+    <q-btn color="white" text-color="black" label="Standard" />
+  </q-page>
+</template>
+
+<script>
+export default {
+  // name: 'PageName',
+}
+</script>
